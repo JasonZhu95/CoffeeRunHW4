b90fb6d5ba396bef8b8e0e76bb33870163a31f4f
JasonZhu95 <68755176+JasonZhu95@users.noreply.github.com>
2021-10-10 19:03:39 -0700

    Added Project Files

---
+++ scripts/displayName.js
@@ -0,0 +1,6 @@
+function displayName() {
+  document.getElementById('display').innerHTML = document.getElementById("name").value;
+
+  var titleOutput = $("input:radio[name=title]:checked").val();
+  document.getElementById('titleOutput').innerHTML = titleOutput;
+}
